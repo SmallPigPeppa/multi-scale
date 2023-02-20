23f04a6abd35cbe7af9134e3b5a2d13c0a18898c
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-20 22:13:56 +0800

    initial

--- scripts/baseline_val.sh
+++ scripts/baseline_val.sh
@@ -1,6 +1,7 @@
 python3 main_baseline.py \
     --data_dir /share/wenzhuoliu/torch_ds/imagenet \
-    --name baseline-supervised
+    --name baseline-supervised \
+    --ckpt_dir baseline-supervised
 
 
 
