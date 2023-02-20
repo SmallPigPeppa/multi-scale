821f6c2f620edb434d5229493518c3043c8d7411
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-20 22:14:29 +0800

    initial

--- scripts/baseline_train.sh
+++ scripts/baseline_train.sh
@@ -3,7 +3,10 @@ DATA_PATH=/mnt/mmtech01/dataset/lzy/ILSVRC2012
 /root/miniconda3/envs/solo-learn/bin/python baseline_train.py \
     --data_dir ${DATA_PATH} \
     --num_gpus 4 \
-    --num_workers 4
+    --num_workers 4 \
+    --name baseline-supervised \
+    --ckpt_dir baseline-supervised
+
 
 
 
