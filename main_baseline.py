35884fffaccc319cb2debf119b867059422c258b
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-20 21:48:51 +0800

    initial

--- main_baseline.py
+++ main_baseline.py
@@ -114,4 +114,4 @@ if __name__ == '__main__':
         batch_size=args.batch_size)
 
     # trainer.validate(model, datamodule=dali_datamodule)
-    trainer.test(model, datamodule=dali_datamodule)
+    trainer.validate(model, datamodule=dali_datamodule)
