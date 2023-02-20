3f9dd3d39153dfa1d874364ce50fce936f0758c8
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-20 23:45:07 +0800

    initial

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,6 +1,9 @@
 python3 main_ms.py \
-    --data_dir /share/wenzhuoliu/torch_ds/imagenet
-
+    --data_dir /share/wenzhuoliu/torch_ds/imagenet\
+    --num_gpus 2 \
+    --num_workers 16 \
+    --ckpt_dir supervised-l1-ckpt\
+    --name supervised-l1
 
 
 
--- scripts/ms_train.sh
+++ scripts/ms_train.sh
@@ -1,6 +1,6 @@
 cd /mnt/mmtech01/usr/liuwenzhuo/code/multi-scale
 DATA_PATH=/mnt/mmtech01/dataset/lzy/ILSVRC2012
-/root/miniconda3/envs/solo-learn/bin/python main_ms.py \
+/root/miniconda3/envs/solo-learn-A100/bin/python main_ms.py \
     --data_dir ${DATA_PATH} \
     --num_gpus 2 \
     --num_workers 16 \
