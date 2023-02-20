669e54afc51ad6cbabfd4a49a54a41e394697465
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-20 20:24:21 +0800

    initial

--- main_ms.py
+++ main_ms.py
@@ -33,7 +33,7 @@ class MSNetPL(pl.LightningModule):
         return z1, z2, z3, y1, y2, y3
 
     def share_step(self, batch, batch_idx):
-        x, target = batch['data'],batch['label']
+        x, target = batch
         z1, z2, z3, y1, y2, y3 = self.forward(x)
 
         si_loss1 = self.mse_loss(z1, z2)
