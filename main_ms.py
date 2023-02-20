4bec8f63db23077dad6ba18b47105f0af9de94e8
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-20 19:02:32 +0800

    initial

--- main_ms.py
+++ main_ms.py
@@ -33,7 +33,7 @@ class MSNetPL(pl.LightningModule):
 
 
     def share_step(self, batch, batch_idx):
-        x, target = batch
+        [x, target] = batch
         z1, z2, z3, y1, y2, y3 = self.forward(x)
 
 
