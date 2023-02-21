e0ba6cd09de37caee018a199c3c13e7f983d0926
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-21 13:56:57 +0800

    initial

--- scripts/ms_train.sh
+++ scripts/ms_train.sh
@@ -3,7 +3,7 @@ DATA_PATH=/mnt/mmtech01/dataset/lzy/ILSVRC2012
 /root/miniconda3/envs/test2/bin/python main_ms.py \
     --data_dir ${DATA_PATH} \
     --num_gpus 2 \
-    --num_workers 16 \
-    --batch_size 256 \
+    --num_workers 32 \
+    --batch_size 128 \
     --ckpt_dir supervised-l1-ckpt\
     --name supervised-l1
