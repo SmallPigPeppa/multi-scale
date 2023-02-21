15823438761d836d07d5e8e7b1b839926194a061
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-21 13:46:28 +0800

    initial

--- scripts/ms_train.sh
+++ scripts/ms_train.sh
@@ -1,9 +1,8 @@
 cd /mnt/mmtech01/usr/liuwenzhuo/code/multi-scale
 DATA_PATH=/mnt/mmtech01/dataset/lzy/ILSVRC2012
-/root/miniconda3/envs/solo-learn/bin/python debug.py \
+/root/miniconda3/envs/test2/bin/python debug.py \
     --data_dir ${DATA_PATH} \
     --num_gpus 2 \
     --num_workers 16 \
     --ckpt_dir supervised-l1-ckpt\
     --name supervised-l1 \
-    --offline
