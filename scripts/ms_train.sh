42a95bd60f2ddc77e5e8f74382005696952c9738
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-21 17:55:48 +0800

    initial

--- scripts/ms_train.sh
+++ scripts/ms_train.sh
@@ -3,7 +3,7 @@ DATA_PATH=/mnt/mmtech01/dataset/lzy/ILSVRC2012
 /root/miniconda3/envs/solo-learn-A100/bin/python main_ms.py \
     --data_dir ${DATA_PATH} \
     --num_gpus 4 \
-    --num_workers 32 \
+    --num_workers 8 \
     --batch_size 128 \
     --ckpt_dir supervised-l1-ckpt\
     --name supervised-l1
