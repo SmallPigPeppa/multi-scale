8073550039ef7e513a2d3ba51a863a3f40eef8e2
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-21 13:59:36 +0800

    initial

--- scripts/ms_train.sh
+++ scripts/ms_train.sh
@@ -2,7 +2,7 @@ cd /mnt/mmtech01/usr/liuwenzhuo/code/multi-scale
 DATA_PATH=/mnt/mmtech01/dataset/lzy/ILSVRC2012
 /root/miniconda3/envs/test2/bin/python main_ms.py \
     --data_dir ${DATA_PATH} \
-    --num_gpus 2 \
+    --num_gpus 4 \
     --num_workers 32 \
     --batch_size 128 \
     --ckpt_dir supervised-l1-ckpt\
