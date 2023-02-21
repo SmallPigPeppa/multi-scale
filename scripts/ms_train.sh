89fd4bd9c02f9eabb15c37d1248a9100c9111555
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-21 14:00:05 +0800

    initial

--- scripts/ms_train.sh
+++ scripts/ms_train.sh
@@ -1,6 +1,6 @@
 cd /mnt/mmtech01/usr/liuwenzhuo/code/multi-scale
 DATA_PATH=/mnt/mmtech01/dataset/lzy/ILSVRC2012
-/root/miniconda3/envs/test2/bin/python main_ms.py \
+/root/miniconda3/envs/solo-learn-A100/bin/python main_ms.py \
     --data_dir ${DATA_PATH} \
     --num_gpus 4 \
     --num_workers 32 \
