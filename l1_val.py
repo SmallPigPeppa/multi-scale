d9fa5efda5af37f1a1e214486ef10c576a995ab5
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-24 02:15:24 +0800

    initial

--- l1_val.py
+++ l1_val.py
@@ -71,8 +71,8 @@ class MSNetValPL(pl.LightningModule):
             acc3_list.append(avg_acc3_size_i)
             acc_best_list.append(avg_acc_best_size_i)
 
-        self.columns = ['size']+[str(i) for i in self.size_list]
-        self.acc_table = [['acc1']+acc1_list, ['acc2']+acc2_list, ['acc3']+acc3_list, ['acc_best']+acc_best_list]
+        self.columns = [str(i) for i in self.size_list]+['size']
+        self.acc_table = [acc1_list+['acc1'], acc2_list+['acc2'], acc3_list+['acc3'], acc_best_list+['acc_best']]
         # self.log_table(key='acc', columns=columns, data=data)
         # self.log(name='acc1', value= torch.tensor(acc1_list), on_step=False, prog_bar=False,reduce_fx=lambda x:x)
         # self.log(name='acc2', value= torch.tensor(acc2_list), on_step=False, prog_bar=False)
