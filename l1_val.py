0e85343f59779a8fefc265880e9d60421529acd5
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-24 01:56:35 +0800

    initial

--- l1_val.py
+++ l1_val.py
@@ -71,9 +71,9 @@ class MSNetValPL(pl.LightningModule):
             acc3_list.append(avg_acc3_size_i)
             acc_best_list.append(avg_acc_best_size_i)
 
-        columns = ["acc1", "acc2","acc3","acc_best"]
-        data=[acc1_list,acc2_list,acc3_list,acc_best_list]
-        self.log_table(key='acc',columns=columns,data=data)
+        self.columns = ["acc1", "acc2", "acc3", "acc_best"]
+        self.acc_table = [acc1_list, acc2_list, acc3_list, acc_best_list]
+        # self.log_table(key='acc', columns=columns, data=data)
         # self.log(name='acc1', value= torch.tensor(acc1_list), on_step=False, prog_bar=False,reduce_fx=lambda x:x)
         # self.log(name='acc2', value= torch.tensor(acc2_list), on_step=False, prog_bar=False)
         # self.log(name='acc3', value= torch.tensor(acc3_list), on_step=False, prog_bar=False)
@@ -121,3 +121,4 @@ if __name__ == '__main__':
         batch_size=args.batch_size)
 
     trainer.validate(model, datamodule=dali_datamodule)
+    wandb_logger.log_table(key="acc", columns=model.columns, data=model.acc_table)
