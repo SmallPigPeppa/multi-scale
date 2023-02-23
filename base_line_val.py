77678ec3afa0041e0968cd482a311b88d89b090d
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-24 01:01:05 +0800

    initial

---
+++ base_line_val.py
@@ -0,0 +1,106 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.callbacks import ModelCheckpoint
+from models.msnet_l1 import MultiScaleNet
+from data_modules.imagenet_dali import ClassificationDALIDataModule
+from data_modules.not_dali import prepare_data
+from pytorch_lightning.strategies.ddp import DDPStrategy
+from args import parse_args
+from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
+from baseline_train import BaselineNetPL
+
+import torch.nn.functional as F
+
+
+class MSNetValPL(pl.LightningModule):
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+        # self.ms_model = MSNetPL(args)
+        # self.ms_model = self.ms_model.load_from_checkpoint(args.val_ckpt_path)
+        self.encoder = BaselineNetPL.load_from_checkpoint(checkpoint_path=args.val_ckpt_path, args=args).encoder
+        self.size_list = list(range(args.start_size, args.end_size, args.interval))
+
+    def forward(self, x):
+        y1, y2, y3 = self.encoder(x)
+        return y3
+
+    def share_step(self, x, target):
+        y = self.forward(x)
+        acc = (torch.argmax(y, dim=1) == target).float().mean()
+
+        result_dict = {
+            'acc':acc
+        }
+        return result_dict
+
+    def validation_step(self, batch, batch_idx):
+        x, target = batch
+        dict_list = []
+        for size_i in self.size_list:
+            x_size_i = F.interpolate(x, size=int(size_i), mode='bilinear')
+            dict_i = self.share_step(x_size_i, target)
+            dict_size_i = {f'{str(size_i)}_{k}': v for k, v in dict_i.items()}
+            dict_list.append(dict_size_i)
+
+        all_size_dict = {k: v for d in dict_list for k, v in d.items()}
+        # self.log_dict(all_size_dict)
+        return all_size_dict
+
+    def validation_epoch_end(self, outputs):
+        acc_list = []
+        for size_i in self.size_list:
+            avg_acc_size_i = sum([output[f"{size_i}_acc"] for output in outputs]) / len(outputs)
+            acc_list.append(avg_acc_size_i)
+
+
+        self.log(value={"acc1": acc_list, "x_axis": self.size_list}, on_step=False, prog_bar=False)
+
+
+
+if __name__ == '__main__':
+    pl.seed_everything(5)
+    args = parse_args()
+    val_ckpt_path = args.val_ckpt_path
+    model = MSNetValPL(args)
+    wandb_logger = WandbLogger(name=args.name, project=args.project, entity=args.entity, offline=args.offline)
+    wandb_logger.watch(model, log="gradients", log_freq=100)
+    wandb_logger.log_hyperparams(args)
+    checkpoint_callback = ModelCheckpoint(dirpath=args.ckpt_dir, save_last=True, save_top_k=2, monitor="val_acc3")
+    trainer = pl.Trainer(gpus=args.num_gpus,
+                         max_epochs=args.max_epochs,
+                         check_val_every_n_epoch=5,
+                         strategy=DDPStrategy(find_unused_parameters=False),
+                         precision=16,
+                         gradient_clip_val=0.5,
+                         logger=wandb_logger,
+                         callbacks=[LearningRateMonitor(logging_interval="step"), checkpoint_callback])
+
+    try:
+        from pytorch_lightning.loops import FitLoop
+
+
+        class WorkaroundFitLoop(FitLoop):
+            @property
+            def prefetch_batches(self) -> int:
+                return 1
+
+
+        trainer.fit_loop = WorkaroundFitLoop(
+            trainer.fit_loop.min_epochs, trainer.fit_loop.max_epochs
+        )
+    except:
+        pass
+
+    dali_datamodule = ClassificationDALIDataModule(
+        train_data_path=os.path.join(args.data_dir, 'train'),
+        val_data_path=os.path.join(args.data_dir, 'val'),
+        num_workers=args.num_workers,
+        batch_size=args.batch_size)
+
+    trainer.validate(model, datamodule=dali_datamodule)
