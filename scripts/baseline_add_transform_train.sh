df2aad409f1b8fcbaf51f388f62ea0a05c19dbe7
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-25 01:18:52 +0800

    initial

---
+++ scripts/baseline_add_transform_train.sh
@@ -0,0 +1,21 @@
+cd /mnt/mmtech01/usr/liuwenzhuo/code/multi-scale
+DATA_PATH=/mnt/mmtech01/dataset/lzy/ILSVRC2012
+/root/miniconda3/envs/solo-learn/bin/python baseline_train.py \
+    --data_dir ${DATA_PATH} \
+    --num_gpus 2 \
+    --num_workers 8 \
+    --name baseline-supervised \
+    --ckpt_dir baseline-supervised
+
+cd /mnt/mmtech01/usr/liuwenzhuo/code/multi-scale
+DATA_PATH=/mnt/mmtech01/dataset/lzy/ILSVRC2012
+/root/miniconda3/envs/solo-learn-A100/bin/python baseline_add_transform_train.py \
+    --data_dir ${DATA_PATH} \
+    --num_gpus 8 \
+    --num_workers 8 \
+    --batch_size 128 \
+    --ckpt_dir supervised-baseline-add-transform-ckpt\
+    --name supervised-baseline-add-transform
+
+
+
