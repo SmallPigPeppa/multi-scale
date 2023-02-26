7a7cdd1e3eda9f5224e99624dcdd21ac517d8e9b
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-26 15:08:33 +0800

    initial

---
+++ scripts/l2_val.sh
@@ -0,0 +1,11 @@
+cd /mnt/mmtech01/usr/liuwenzhuo/code/multi-scale
+DATA_PATH=/mnt/mmtech01/dataset/lzy/ILSVRC2012
+/root/miniconda3/envs/solo-learn-A100/bin/python l1_val.py \
+    --data_dir ${DATA_PATH} \
+    --num_gpus 1 \
+    --num_workers 8 \
+    --batch_size 128 \
+    --ckpt_dir supervised-l2-ckpt \
+    --name supervised-l2 \
+    --project Multi-Scale-Net-val \
+    --val_ckpt_path supervised-l2-ckpt/last.ckpt
