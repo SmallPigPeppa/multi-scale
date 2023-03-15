460b9c1c5cd0cc294b06b4e0a03f87c90bf321ba
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-15 11:58:05 +0800

    initial

---
+++ scripts/baseline_add_aug_val.sh
@@ -0,0 +1,19 @@
+#python3 main_baseline.py \
+#    --data_dir /share/wenzhuoliu/torch_ds/imagenet \
+#    --name baseline-supervised \
+#    --ckpt_dir baseline-supervised
+
+
+
+
+cd /mnt/mmtech01/usr/liuwenzhuo/code/multi-scale
+DATA_PATH=/mnt/mmtech01/dataset/lzy/ILSVRC2012
+/root/miniconda3/envs/solo-learn-A100/bin/python baseline_val.py \
+    --data_dir ${DATA_PATH} \
+    --num_gpus 1 \
+    --num_workers 8 \
+    --batch_size 128 \
+    --ckpt_dir supervised-baseline-ckpt \
+    --name supervised-baseline-add-aug \
+    --project Multi-Scale-Net-val \
+    --val_ckpt_path supervised-baseline-add-transform-ckpt/epoch=99-step=125100.ckpt
